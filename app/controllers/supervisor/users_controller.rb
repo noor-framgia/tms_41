ec05613a077283be7fd12ac13db3d960af4cf1e4
gittiggit <contactwithmahmud@gmail.com>
2015-12-10 10:03:19 +0600

    Commited second time

---
+++ app/controllers/supervisor/users_controller.rb
@@ -0,0 +1,28 @@
+class Supervisor::UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :destroy]
+
+  def index
+    @users = User.supervisor
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+   if @user.destroy
+     flash[:success] = t(:success_message_delete)
+     redirect_to supervisor_users_path
+   end
+  end
+
+  private
+    def load_user
+      @user = User.find params[:id]
+    end
+end
